1026b0e836add5013ae2f759111f4e5e609d4e22
cdcummings10 <ccummings0913@gmail.com>
2019-10-16 08:50:41 -0700

    Finished AddToBalance method, created tests for view balance and add to balance methods.

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -17,18 +17,41 @@ namespace ATM
         private static void StartSequence()
         {
             double balance = 5000.00;
-            ViewBalance(balance);
-        }
+            bool atmOn = true;
+            while (atmOn)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        ViewBalance(balance);
+                        break;
+                    case "2":
+                        Console.WriteLine("How much would you like to deposit?");
+                        Console.Write("Amount: ");
+                        string deposit = Console.ReadLine();
+                        balance = AddToBalance(balance, Convert.ToDouble(deposit));
+                        break;
+                    case "3":
+                        Console.WriteLine("How much would you like to withdraw?");
+                        break;
+                    case "4":
+                        Console.WriteLine("Thanks for banking with Shitake Bank!");
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                        break;
+                }
+                }
+            }
 
-        public static double ViewBalance(double balance)
-        {
-            Console.WriteLine($"Your current balance: ${balance}");
-            return balance;
-        }
+            public static double ViewBalance(double balance)
+            {
+                Console.WriteLine($"Your current balance: ${balance}");
+                return balance;
+            }
 
-        public static double AddToBalance(double balance, double addition)
-        {
-            return balance;
+            public static double AddToBalance(double balance, double addition)
+            {
+                return addition + balance;
+            }
         }
     }
-}
--- ATM/XUnitTestProject1/UnitTest1.cs
+++ ATM/XUnitTestProject1/UnitTest1.cs
@@ -11,5 +11,21 @@ namespace XUnitTestProject1
         {
             Assert.Equal(5000.00, ViewBalance(5000.00));
         }
+
+        [Fact]
+        public void BalanceReturndIsCorrect()
+        {
+            Assert.NotEqual(4500, ViewBalance(5000.00));
+        }
+
+        [Theory]
+        [InlineData(4500.00, 500.00)]
+        [InlineData(4750.00, 250.00)]
+        [InlineData(4399.45, 600.55)]
+        [InlineData(4989.01, 10.99)]
+        public void ReturnNewBalanceAfterAddition(double balance, double addition)
+        {
+            Assert.Equal(5000.00, AddToBalance(balance, addition));
+        }
     }
 }
